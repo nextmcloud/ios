219589309f3a873ab99f0f306c410c783b721acc
Shweta Waikar <shweta.waikar@t-systems.com>
2023-04-26 13:22:23 +0530

    Removed Images

--- Brand/NCBrand.swift
+++ Brand/NCBrand.swift
@@ -34,7 +34,7 @@ import UIKit
     @objc public var brand: String = "Nextcloud"
     @objc public var textCopyrightNextcloudiOS: String = "Nextcloud Liquid for iOS %@ © 2023"
     @objc public var textCopyrightNextcloudServer: String = "Nextcloud Server %@"
-    @objc public var loginBaseUrl: String = "https://cloud.nextcloud.com"
+    @objc public var loginBaseUrl: String = "https://magentacloud.de"
     @objc public var pushNotificationServerProxy: String = "https://push-notifications.nextcloud.com"
     @objc public var linkLoginHost: String = "https://nextcloud.com/install"
     @objc public var linkloginPreferredProviders: String = "https://nextcloud.com/signup-ios"
