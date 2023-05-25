334654810d54a25de08e061f303400e9c214c3e8
Shweta Waikar <shweta.waikar@t-systems.com>
2023-05-25 17:19:57 +0530

    NMC 1998 Tracking tealium adjust setup

--- iOSClient/AppDelegate.swift
+++ iOSClient/AppDelegate.swift
@@ -35,6 +35,8 @@ import RealmSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     var backgroundSessionCompletionHandler: (() -> Void)?
+    var taskAutoUploadDate: Date = Date()
+    @objc let adjust = AdjustHelper()
     var isUiTestingEnabled: Bool {
         return ProcessInfo.processInfo.arguments.contains("UI_TESTING")
     }
@@ -124,6 +126,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         _ = NCDownloadAction.shared
         _ = NCNetworkingProcess.shared
 
+        if account.isEmpty {
+            if NCBrandOptions.shared.disable_intro {
+                openLogin(viewController: nil, selector: NCGlobal.shared.introLogin, openLoginWeb: false)
+            } else {
+                if let viewController = UIStoryboard(name: "NCIntro", bundle: nil).instantiateInitialViewController() {
+                    let navigationController = NCLoginNavigationController(rootViewController: viewController)
+                    window?.rootViewController = navigationController
+                    window?.makeKeyAndVisible()
+                }
+            }
+        } else {
+            NCPasscode.shared.presentPasscode(delegate: self) {
+                NCPasscode.shared.enableTouchFaceID()
+            }
+        }
+        adjust.configAdjust()
+        adjust.subsessionStart()
+        TealiumHelper.shared.start()
         return true
     }
 
