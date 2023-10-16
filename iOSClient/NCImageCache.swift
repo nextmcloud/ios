11f90ce788defde250a07ef6b779ad3ac34a46ff
TSI-amrutwaghmare <96108296+TSI-amrutwaghmare@users.noreply.github.com>
2023-10-16 17:38:31 +0530

    NMC 2611 - Branding changes

--- iOSClient/NCImageCache.swift
+++ iOSClient/NCImageCache.swift
@@ -5,8 +5,8 @@
 import Foundation
 import UIKit
 
-final class NCImageCache: @unchecked Sendable {
-    static let shared = NCImageCache()
+@objc class NCImageCache: NSObject {
+    @objc static let shared = NCImageCache()
 
     private let utility = NCUtility()
     private let cache = NSCache<NSString, UIImage>()
@@ -382,6 +382,10 @@ final class NCImageCache: @unchecked Sendable {
 //        images.buttonTrash = UIImage(named: "trashIcon")!.image(color: NCBrandColor.shared.iconImageColor, size: 24)//50)
 
         createImagesBrandCache()
+    }
+
+    func getImageShared(account: String) -> UIImage {
+        return UIImage(named: "share")!.image(color: NCBrandColor.shared.getElement(account: account))
     }
 
     func getImageCanShare() -> UIImage {
