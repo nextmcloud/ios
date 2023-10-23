ad182646190f06f81e1e477ddc20dfdf651a6142
TSI-amrutwaghmare <96108296+TSI-amrutwaghmare@users.noreply.github.com>
2023-10-23 16:49:46 +0530

    NMC 2261 - Audio record and upload customisation

--- iOSClient/AudioRecorder/NCAudioRecorderViewController.swift
+++ iOSClient/AudioRecorder/NCAudioRecorderViewController.swift
@@ -54,7 +54,7 @@ class NCAudioRecorderViewController: UIViewController, NCAudioRecorderDelegate {
 
         view.backgroundColor = .clear
         contentContainerView.backgroundColor = UIColor.lightGray
-        voiceRecordHUD.fillColor = UIColor.green
+        voiceRecordHUD.fillColor = NCBrandColor.shared.progressColorGreen60
 
         Task {
             self.fileName = await NCNetworking.shared.createFileName(fileNameBase: NSLocalizedString("_untitled_", comment: "") + ".m4a", account: self.session.account, serverUrl: controller.currentServerUrl())
@@ -134,7 +134,7 @@ class NCAudioRecorderViewController: UIViewController, NCAudioRecorderDelegate {
         }
 
         voiceRecordHUD.update(CGFloat(rate))
-        voiceRecordHUD.fillColor = UIColor.green
+        voiceRecordHUD.fillColor = NCBrandColor.shared.progressColorGreen60
 
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.second]
