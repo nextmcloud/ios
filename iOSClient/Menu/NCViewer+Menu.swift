a34c0b9b512c2cc7df265bd1175c7c6ff2ffa101
TSI-amrutwaghmare <96108296+TSI-amrutwaghmare@users.noreply.github.com>
2023-10-30 15:57:57 +0530

    NMC 2253 - Context menu customisation changes

--- iOSClient/Menu/NCViewer+Menu.swift
+++ iOSClient/Menu/NCViewer+Menu.swift
@@ -162,6 +162,11 @@ extension NCViewer {
                 )
             )
         }
+        // COPY IN PASTEBOARD
+        //
+        if !webView, metadata.isCopyableInPasteboard {
+            actions.append(.copyAction(selectOcId: [metadata.ocId]))
+        }
 
         //
         // PDF
