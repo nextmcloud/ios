4660ffc9cfb7a69019522b3d39dc1bfa73a4d15b
TSI-amrutwaghmare <96108296+TSI-amrutwaghmare@users.noreply.github.com>
2023-11-01 15:13:42 +0530

    NMC 2237 - More option tab customisation changes

--- iOSClient/More/Cells/BaseNCMoreCell.swift
+++ iOSClient/More/Cells/BaseNCMoreCell.swift
@@ -28,22 +28,6 @@ class BaseNCMoreCell: UITableViewCell {
     let selectionColor: UIView = UIView()
     let defaultCornerRadius: CGFloat = 10.0
 
-    override var frame: CGRect {
-        get {
-            return super.frame
-        }
-        set (newFrame) {
-            var frame = newFrame
-            let newWidth = frame.width * 0.90
-            let space = (frame.width - newWidth) / 2
-            frame.size.width = newWidth
-            frame.origin.x += space
-            super.frame = frame
-        }
-    }
-
-    open func setupCell(account: String, controller: NCMainTabBarController?) {}
-
     override func awakeFromNib() {
         super.awakeFromNib()
 
