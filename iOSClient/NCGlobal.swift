b573c74a572315a4a4653fe1fb9865744cef4361
TSI-amrutwaghmare <96108296+TSI-amrutwaghmare@users.noreply.github.com>
2023-11-27 17:19:30 +0530

    NMC 1997 - Sharing customisation
    NMC 1997 - Merging conflict resolve after nextcloud version 4.9.2 release
    
    NMC 1992 - Remove configuration changes
    
    NMC 1992 - Removed unused files
    
    NMC 1992 - Sharing changes update after NC 5.2
    
    NMC 1992 - Removed project configuration file changes

--- iOSClient/NCGlobal.swift
+++ iOSClient/NCGlobal.swift
@@ -329,6 +329,15 @@ final class NCGlobal: Sendable {
 
     let notificationCenterEnableSwipeGesture                    = "enableSwipeGesture"
     let notificationCenterDisableSwipeGesture                   = "disableSwipeGesture"
+    
+    let notificationCenterShareViewIn                           = "ShareViewIn"
+    let notificationCenterShareAdvancePermission                = "ShareAdvancePermission"
+    let notificationCenterShareSendEmail                        = "ShareSendEmail"
+    let notificationCenterShareUnshare                          = "ShareUnshare"
+    let notificationCenterStatusReadOnly                        = "statusReadOnly"
+    let notificationCenterStatusEditing                         = "statusEditing"
+    let notificationCenterStatusFileDrop                        = "statusFileDrop"
+
 
     let notificationCenterPlayerIsPlaying                       = "playerIsPlaying"
     let notificationCenterPlayerStoppedPlaying                  = "playerStoppedPlaying"
