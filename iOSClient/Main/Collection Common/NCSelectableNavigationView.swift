aa16c7b724d3bea978faa79a1b9787d47709a010
TSI-amrutwaghmare <96108296+TSI-amrutwaghmare@users.noreply.github.com>
2023-11-29 14:46:53 +0530

    NMC 2161 - Menu option images and color theming changes

--- iOSClient/Main/Collection Common/NCSelectableNavigationView.swift
+++ iOSClient/Main/Collection Common/NCSelectableNavigationView.swift
@@ -44,7 +44,6 @@ protocol NCSelectableNavigationView: AnyObject {
     var collectionView: UICollectionView! { get set }
     var isEditMode: Bool { get set }
     var fileSelect: [String] { get set }
-//    var selectIndexPaths: [IndexPath] { get set }
     var appDelegate: AppDelegate { get }
     var selectIndexPaths: [IndexPath] { get set }
     var titleCurrentFolder: String { get }
@@ -59,7 +58,6 @@ protocol NCSelectableNavigationView: AnyObject {
     var tabBarSelect: NCSelectableViewTabBar? { get set }
 
     func reloadDataSource(withQueryDB: Bool)
-//    func reloadDataSource(withQueryDB: Bool)
     func setNavigationLeftItems()
     func setNavigationRightItems(enableMenu: Bool)
     func createMenuActions() -> [NCMenuAction]
