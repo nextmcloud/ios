3b79c4063a1f99fdc575898649631158af0776c8
Amrut Waghmare <amrut.waghmare@t-systems.com>
2024-08-26 11:28:46 +0530

    NMC 1992 - Sharing changes after NC release 5.5.4

--- iOSClient/Share/NCShareCommon.swift
+++ iOSClient/Share/NCShareCommon.swift
@@ -119,6 +119,6 @@ enum NCShareCommon {
     }
     
     func canReshare(withPermission permission: String) -> Bool {
-        return permission.contains(NCGlobal.shared.permissionCanShare)
+        return permission.contains(NCPermissions().permissionCanShare)
     }
 }
