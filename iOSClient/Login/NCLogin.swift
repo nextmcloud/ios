55db979e2b4dd7ca046442608ed9fc89a2b853e7
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2025-04-09 11:55:14 +0530

    NMC 2615 - Login screens customisation changes

--- iOSClient/Login/NCLogin.swift
+++ iOSClient/Login/NCLogin.swift
@@ -330,8 +330,7 @@ class NCLogin: UIViewController, UITextFieldDelegate, NCLoginQRCodeDelegate {
                 if let host = URL(string: url)?.host {
                     NCNetworking.shared.writeCertificate(host: host)
                 }
-                let loginOptions = NKRequestOptions(customUserAgent: userAgent)
-                NextcloudKit.shared.getLoginFlowV2(serverUrl: url, options: loginOptions) { [self] token, endpoint, login, _, error in
+                NextcloudKit.shared.getLoginFlowV2(serverUrl: url) { [self] token, endpoint, login, _, error in                    
                     // Login Flow V2
 //                    if error == .success, let token, let endpoint, let login {
 //                        nkLog(debug: "Successfully received login flow information.")
