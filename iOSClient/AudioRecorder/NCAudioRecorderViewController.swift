949551f4dbd65cbdc4a55da86df9a687814fbc00
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2025-04-09 17:06:20 +0530

    NMC 2261 - Audio recorder and upload customisation

--- iOSClient/AudioRecorder/NCAudioRecorderViewController.swift
+++ iOSClient/AudioRecorder/NCAudioRecorderViewController.swift
@@ -88,8 +88,8 @@ class NCAudioRecorderViewController: UIViewController, NCAudioRecorderDelegate {
                 guard let navigationController = UIStoryboard(name: "NCCreateFormUploadVoiceNote", bundle: nil).instantiateInitialViewController() as? UINavigationController,
                       let viewController = navigationController.topViewController as? NCCreateFormUploadVoiceNote else { return }
                 navigationController.modalPresentationStyle = .formSheet
-                viewController.setup(serverUrl: self.appDelegate.activeServerUrl, fileNamePath: NSTemporaryDirectory() + self.fileName, fileName: self.fileName)
-                self.appDelegate.window?.rootViewController?.present(navigationController, animated: true)
+                viewController.setup(serverUrl: controller.currentServerUrl(), fileNamePath: NSTemporaryDirectory() + self.fileName, fileName: self.fileName)
+                UIApplication.shared.firstWindow?.rootViewController?.present(navigationController, animated: true)
             }
         } else {
             do {
@@ -108,7 +108,7 @@ class NCAudioRecorderViewController: UIViewController, NCAudioRecorderDelegate {
                                                               ocId: UUID().uuidString,
                                                               serverUrl: controller.currentServerUrl(),
                                                               session: self.session,
-                                                              sceneIdentifier: self.controller?.sceneIdentifier)
+                                                              sceneIdentifier: self.appDelegate.sceneIdentifier)
 
         metadata.session = NCNetworking.shared.sessionUploadBackground
         metadata.sessionSelector = NCGlobal.shared.selectorUploadFile
