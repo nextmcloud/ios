14d1d04c2373df20b416adfa5347c46771f92c28
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2025-04-10 15:45:28 +0530

    NMC 1937 - E2EE customisation changes

--- iOSClient/Settings/E2EE/NCManageE2EEView.swift
+++ iOSClient/Settings/E2EE/NCManageE2EEView.swift
@@ -24,6 +24,18 @@
 import SwiftUI
 import NextcloudKit
 
+@objc class NCManageE2EEInterface: NSObject {
+
+    @objc func makeShipDetailsUI(account: String) -> UIViewController {
+
+        let controller = UIApplication.shared.firstWindow?.rootViewController as? NCMainTabBarController
+        let details = NCManageE2EEView(model: NCManageE2EE(controller: controller))
+        let vc = UIHostingController(rootView: details)
+        vc.title = NSLocalizedString("_e2e_settings_", comment: "")
+        return vc
+    }
+}
+
 struct NCManageE2EEView: View {
     @ObservedObject var model: NCManageE2EE
     @Environment(\.presentationMode) var presentationMode
@@ -32,84 +44,52 @@ struct NCManageE2EEView: View {
         VStack {
             if model.isEndToEndEnabled {
                 List {
-                    Section(header: Text(""), footer: Text(model.statusOfService + "\n\n" + "End-to-End Encryption " + model.capabilities.capabilityE2EEApiVersion)) {
+                    Section(header: Text(""), footer: Text("End-to-End Encryption " + NCGlobal.shared.capabilityE2EEApiVersion)) {
                         Label {
                             Text(NSLocalizedString("_e2e_settings_activated_", comment: ""))
                         } icon: {
                             Image(systemName: "checkmark.circle.fill")
                                 .resizable()
                                 .scaledToFit()
-                                .font(Font.system(.body).weight(.light))
-                                .frame(width: 25, height: 25)
                                 .foregroundColor(.green)
                         }
                     }
-                    HStack {
+
+                    Section(header: Text(""), footer: Text(NSLocalizedString("_read_passphrase_description_", comment: ""))) {
                         Label {
                             Text(NSLocalizedString("_e2e_settings_read_passphrase_", comment: ""))
                         } icon: {
                             Image(systemName: "eye")
                                 .resizable()
                                 .scaledToFit()
-                                .font(Font.system(.body).weight(.light))
-                                .frame(width: 25, height: 25)
-                                .foregroundColor(Color(NCBrandColor.shared.iconImageColor))
+                                .foregroundColor(Color(NCBrandColor.shared.iconColor))
+                                .frame(width: 20, height: 30)
                         }
-                        Spacer()
-                    }
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        if NCKeychain().passcode != nil {
-                            model.requestPasscodeType("readPassphrase")
-                        } else {
-                            NCContentPresenter().showInfo(error: NKError(errorCode: 0, errorDescription: "_e2e_settings_lock_not_active_"))
+                        .onTapGesture {
+                            if NCKeychain().passcode != nil {
+                                model.requestPasscodeType("readPassphrase")
+                            } else {
+                                NCContentPresenter().showInfo(error: NKError(errorCode: 0, errorDescription: "_e2e_settings_lock_not_active_"))
+                            }
                         }
                     }
-                    HStack {
+
+                    let removeStrDesc1 = NSLocalizedString("_remove_passphrase_desc_1_", comment: "")
+                    let removeStrDesc2 = NSLocalizedString("_remove_passphrase_desc_2_", comment: "")
+                    let removeStrDesc = String(format: "%@\n\n%@", removeStrDesc1, removeStrDesc2)
+                    Section(header: Text(""), footer: Text(removeStrDesc)) {
                         Label {
                             Text(NSLocalizedString("_e2e_settings_remove_", comment: ""))
                         } icon: {
-                            Image(systemName: "xmark")
+                            Image(systemName: "trash")
                                 .resizable()
                                 .scaledToFit()
-                                .font(Font.system(.body).weight(.light))
-                                .frame(width: 25, height: 15)
-                                .foregroundColor(Color(NCBrandColor.shared.iconImageColor))
+                                .foregroundColor(Color(NCBrandColor.shared.iconColor))
+                                .frame(width: 20, height: 30)
                         }
-                        Spacer()
-                    }
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        if NCKeychain().passcode != nil {
-                            model.requestPasscodeType("removeLocallyEncryption")
-                        } else {
-                            NCContentPresenter().showInfo(error: NKError(errorCode: 0, errorDescription: "_e2e_settings_lock_not_active_"))
-                        }
-                    }
-#if DEBUG
-                    deleteCerificateSection
-#endif
-                }
-            } else {
-                List {
-                    Section(header: Text(""), footer: Text(model.statusOfService + "\n\n" + "End-to-End Encryption " + model.capabilities.capabilityE2EEApiVersion)) {
-                        HStack {
-                            Label {
-                                Text(NSLocalizedString("_e2e_settings_start_", comment: ""))
-                            } icon: {
-                                Image(systemName: "play.circle")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .font(Font.system(.body).weight(.light))
-                                    .frame(width: 25, height: 25)
-                                    .foregroundColor(.green)
-                            }
-                            Spacer()
-                        }
-                        .contentShape(Rectangle())
                         .onTapGesture {
                             if NCKeychain().passcode != nil {
-                                model.requestPasscodeType("startE2E")
+                                model.requestPasscodeType("removeLocallyEncryption")
                             } else {
                                 NCContentPresenter().showInfo(error: NKError(errorCode: 0, errorDescription: "_e2e_settings_lock_not_active_"))
                             }
@@ -119,17 +99,40 @@ struct NCManageE2EEView: View {
                     deleteCerificateSection
 #endif
                 }
+
+            } else {
+
+                List {
+                    let startE2EDesc1 = NSLocalizedString("_start_e2e_encryption_1_", comment: "");
+                    let startE2EDesc2 = NSLocalizedString("_start_e2e_encryption_2_", comment: "");
+                    let startE2EDesc3 = NSLocalizedString("_start_e2e_encryption_3_", comment: "");
+                    let startE2EDesc  = String(format: "%@\n\n%@\n\n%@",startE2EDesc1,startE2EDesc2,startE2EDesc3)
+                    Section(header: Text(""), footer: Text(startE2EDesc)) {
+                        HStack {
+                            Label {
+                                Text(NSLocalizedString("_e2e_settings_start_", comment: ""))
+                            } icon: {
+                            }
+                            Spacer()
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            if let passcode = NCKeychain().passcode {
+                                model.requestPasscodeType("startE2E")
+                            } else {
+                                NCContentPresenter().showInfo(error: NKError(errorCode: 0, errorDescription: "_e2e_settings_lock_not_active_"))
+                            }
+                        }
+                    }
+
+#if DEBUG
+                    deleteCerificateSection
+#endif
+                }
+                .listStyle(GroupedListStyle())
             }
         }
-        .navigationBarTitle(NSLocalizedString("_e2e_settings_", comment: ""))
-        .navigationBarTitleDisplayMode(.inline)
         .background(Color(UIColor.systemGroupedBackground))
-        .defaultViewModifier(model)
-        .onChange(of: model.navigateBack) { newValue in
-            if newValue {
-                presentationMode.wrappedValue.dismiss()
-            }
-        }
     }
 
     @ViewBuilder
@@ -144,7 +147,7 @@ struct NCManageE2EEView: View {
                         .scaledToFit()
                         .font(Font.system(.body).weight(.light))
                         .frame(width: 25, height: 25)
-                        .foregroundColor(Color(NCBrandColor.shared.textColor2))
+                        .foregroundColor(Color(UIColor.systemGray))
                 }
                 Spacer()
             }
@@ -167,7 +170,7 @@ struct NCManageE2EEView: View {
                         .scaledToFit()
                         .font(Font.system(.body).weight(.light))
                         .frame(width: 25, height: 25)
-                        .foregroundColor(Color(NCBrandColor.shared.textColor2))
+                        .foregroundColor(Color(UIColor.systemGray))
                 }
                 Spacer()
             }
@@ -186,5 +189,64 @@ struct NCManageE2EEView: View {
 }
 
 #Preview {
-    NCManageE2EEView(model: NCManageE2EE(controller: nil))
+    let controller = UIApplication.shared.firstWindow?.rootViewController as? NCMainTabBarController
+    NCManageE2EEView(model: NCManageE2EE(controller: controller))
+}
+
+// MARK: - Preview / Test
+
+struct SectionView: View {
+
+    @State var height: CGFloat = 0
+    @State var text: String = ""
+
+    var body: some View {
+        HStack {
+            Text(text)
+        }
+        .frame(maxWidth: .infinity, minHeight: height, alignment: .bottomLeading)
+    }
+}
+
+struct NCManageE2EEViewTest: View {
+
+    var body: some View {
+
+        VStack {
+            List {
+                Section(header: SectionView(height: 50, text: "Section Header View")) {
+                    Label {
+                        Text(NSLocalizedString("_e2e_settings_activated_", comment: ""))
+                    } icon: {
+                        Image(systemName: "checkmark.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(.green)
+                    }
+                }
+                Section(header: SectionView(text: "Section Header View 42")) {
+                    Label {
+                        Text(NSLocalizedString("_e2e_settings_activated_", comment: ""))
+                    } icon: {
+                        Image(systemName: "checkmark.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(.red)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct NCManageE2EEView_Previews: PreviewProvider {
+    static var previews: some View {
+
+        // swiftlint:disable force_cast
+        let controller = UIApplication.shared.firstWindow?.rootViewController as? NCMainTabBarController
+        NCManageE2EEView(model: NCManageE2EE(controller: controller))
+        // swiftlint:enable force_cast
+    }
 }
