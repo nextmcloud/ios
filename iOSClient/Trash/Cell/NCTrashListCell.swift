eac1ba7109949c8f81e6b4e804844212ff89cc6c
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2025-04-14 16:57:15 +0530

    NMC 2341 - NCTrashView changes

--- iOSClient/Trash/Cell/NCTrashListCell.swift
+++ iOSClient/Trash/Cell/NCTrashListCell.swift
@@ -60,7 +60,8 @@ class NCTrashListCell: UICollectionViewCell, NCTrashCellProtocol {
 
         ]
 
-        imageRestore.image = utility.loadImage(named: "restore", color: NCBrandColor.shared.iconColor)
+        imageRestore.image = utility.loadImage(named: "restore", colors: [NCBrandColor.shared.iconColor])
+
         imageMore.image = UIImage(systemName: "trash")
         imageMore.tintColor = NCBrandColor.shared.iconColor
 
@@ -137,8 +138,11 @@ class NCTrashListCell: UICollectionViewCell, NCTrashCellProtocol {
         }
         if status {
             var blurEffectView: UIView?
-            blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .systemMaterial))
-            blurEffectView?.backgroundColor = .lightGray
+            var blurEffect: UIVisualEffect?
+            let traitCollectionUserInterfaceStyleDark = traitCollection.userInterfaceStyle == .dark
+            blurEffect = UIBlurEffect(style: traitCollectionUserInterfaceStyleDark ? .dark : .extraLight)
+            blurEffectView = UIVisualEffectView(effect: blurEffect)
+            blurEffectView?.backgroundColor = traitCollectionUserInterfaceStyleDark ? .black : .lightGray
             blurEffectView?.frame = self.bounds
             blurEffectView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
             imageSelect.image = NCImageCache.shared.getImageCheckedYes(color: color)
