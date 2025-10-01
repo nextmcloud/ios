96e2bdae91320dffa812daadf471b7de26d1b742
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2025-10-01 13:56:52 +0530

    NMC 2237 - More option tab customisation changes

--- iOSClient/More/NCMoreNavigationController.swift
+++ iOSClient/More/NCMoreNavigationController.swift
@@ -7,11 +7,10 @@ import SwiftUI
 
 class NCMoreNavigationController: NCMainNavigationController {
     override func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        super.navigationController(navigationController, willShow: viewController, animated: animated)
-
-        guard viewController is NCCollectionViewCommon || viewController is NCActivity || viewController is NCTrash else {
-            setNavigationBarAppearance(backgroundColor: .systemGray6)
-            return
+        if viewController is NCCollectionViewCommon || viewController is NCActivity || viewController is NCTrash {
+            setNavigationBarAppearance()
+        } else {
+            setGroupAppearance()
         }
     }
 
