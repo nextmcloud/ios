181685e87f8d73280682fa655d154832fd08dca8
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2025-10-03 12:07:52 +0530

    NMC 2161 - Menu option changes

--- iOSClient/Main/Collection Common/NCSelectableNavigationView.swift
+++ iOSClient/Main/Collection Common/NCSelectableNavigationView.swift
@@ -40,19 +40,10 @@ public protocol NCSelectableViewTabBar {
 
 protocol NCSelectableNavigationView: AnyObject {
     var viewController: UIViewController { get }
-//    var appDelegate: AppDelegate { get }
     var selectableDataSource: [RealmSwiftObject] { get }
     var collectionView: UICollectionView! { get set }
     var isEditMode: Bool { get set }
     var fileSelect: [String] { get set }
-//    var selectIndexPaths: [IndexPath] { get set }
-    var appDelegate: AppDelegate { get }
-    var selectableDataSource: [RealmSwiftObject] { get }
-    var collectionView: UICollectionView! { get set }
-    var isEditMode: Bool { get set }
-    var selectOcId: [String] { get set }
-    var appDelegate: AppDelegate { get }
-    var selectIndexPaths: [IndexPath] { get set }
     var titleCurrentFolder: String { get }
     var navigationItem: UINavigationItem { get }
     var navigationController: UINavigationController? { get }
@@ -89,9 +80,6 @@ extension NCSelectableNavigationView {
         DispatchQueue.main.async {
             self.isEditMode = isOn ?? !self.isEditMode
             self.fileSelect.removeAll()
-//            self.selectIndexPaths.removeAll()
-            self.selectOcId.removeAll()
-            self.selectIndexPaths.removeAll()
             self.setNavigationLeftItems()
             self.setNavigationRightItems(enableMenu: true)
             self.collectionView.reloadData()
@@ -101,8 +89,6 @@ extension NCSelectableNavigationView {
     func collectionViewSelectAll() {
         
         fileSelect = selectableDataSource.compactMap({ $0.primaryKeyValue })
-//        fileSelect = NCCollectionViewDataSource().getMetadataSourceForAllSections().compactMap({ $0.primaryKeyValue })
-//        selectOcId = selectableDataSource.compactMap({ $0.primaryKeyValue })
         collectionView.reloadData()
         setNavigationRightItems(enableMenu: false)
     }
