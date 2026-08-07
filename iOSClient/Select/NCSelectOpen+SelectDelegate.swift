ddad77b3ade5f9eca25678646e1d0b96e97eab28
harshada-15-tsys <harshada.deshmukh@t-systems.com>
2026-08-07 18:03:44 +0530

    Commenting alert for  Prevent moving an item to the same folder

--- iOSClient/Select/NCSelectOpen+SelectDelegate.swift
+++ iOSClient/Select/NCSelectOpen+SelectDelegate.swift
@@ -20,11 +20,11 @@ final class NCSelectOpen: NCSelectDelegate {
 
                 } else if move {
                     for case let metadata as tableMetadata in items {
-                        // Prevent moving an item to the same folder
-                        if metadata.serverUrl == destination {
-                            await controller?.present(UIAlertController.warning(message: NSLocalizedString("_cannot_move_to_same_folder_", comment: "Cannot move to the same folder")), animated: true)
-                            continue
-                        }
+//                        // Prevent moving an item to the same folder
+//                        if metadata.serverUrl == destination {
+//                            await controller?.present(UIAlertController.warning(message: NSLocalizedString("_cannot_move_to_same_folder_", comment: "Cannot move to the same folder")), animated: true)
+//                            continue
+//                        }
                         if metadata.status != NCGlobal.shared.metadataStatusNormal, metadata.status != NCGlobal.shared.metadataStatusWaitMove {
                             continue
                         }
